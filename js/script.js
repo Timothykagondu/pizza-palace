681d171292b2d61541d11f9440632062da1a0f51
TimothyKagondu <timothy.2k21@gmail.com>
2022-03-21 09:33:56 +0300

    finalization

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 function myFunction(){
-   alert("Timo");
+   var score=0;
+   var numofquestions = 3
+   var ansarr = ['small','olives','crispy'];
 
+   var size = document.forms['quiz'][size].value;
+   var toppings = document.forms['quiz'][toppings].value;
+   var crust = document.forms['quiz'][crust].value;
+
+   for(var i = 1; i <= numofquestions; i++){
+      if(eval('q'+i)== ''){
+         alert("you havent ordered yet" +i);
+      }
+
+      for(var i = 1; i <= numofquestions; i++){
+         if(eval('q'+i)== ansarr[i-1]){
+            score++;
+         }
+      }
+      var results = document.getElementById(results);
+      results.innerHTML="<h2>You pay" + score + numofquestions+
+      alert('you payment'+score+numofquestions)
+   }
 
 }
